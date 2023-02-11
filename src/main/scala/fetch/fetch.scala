31c5066705a0dd688d42274c63418ce8be790b11
Kaveesha-98 <kaveesha.yalegama@gmail.com>
2023-02-11 15:40:04 +0530

    fetch unit with IO defined

---
+++ src/main/scala/fetch/fetch.scala
@@ -0,0 +1,39 @@
+package fetch
+
+import chisel3._
+import chisel3.util._
+import chisel3.experimental.BundleLiterals._
+import chisel3.experimental.IO
+
+// definition of all ports can be found here
+import pipeline.ports._
+
+/**
+  * Question: does the fetch unit need to communicate the predicted next address along with
+  * instruction to the decode unit?
+  * It will be 64-bit that will be rarely used(Only for branch instructions it will be used)
+  */
+
+/**
+  * Functionality - decode unit communicates the pc of the instruction
+  * its epecting through the decodeIssue.expeted port.
+  * 
+  * Results of branch predictions are returned on branchRes port.
+  * 
+  * cache is used to access instruction memory.
+  *
+  */
+class fetch extends Module {
+
+  // interface with cache
+  val cache = IO(new Bundle {
+    val req   = DecoupledIO(UInt(64.W)) // address is 64 bits wide for 64-bit machine
+    val resp  = Flipped(DecoupledIO(UInt(32.W))) // instructions are 32 bits wide
+  })
+
+  // issuing instructions to pc
+  val decodeIssue = IO(new issueInstrFrmFetch)
+
+  // receiving results of branches in order
+  val branchRes   = IO(new branchResToFetch)
+}
