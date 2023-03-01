02506bb04a96c9583a9fc99aeec8d5f15d2baba7
Kaveesha-98 <kaveesha.yalegama@gmail.com>
2023-03-01 13:47:53 +0530

    global configuration file

---
+++ src/main/scala/common/configuration.scala
@@ -0,0 +1,18 @@
+package pipeline.configuration
+
+/**
+  * * * * * IMPORTANT * * * * * 
+  * There is only one maintainer of this file. Only the maintainer is only allowed
+  * to make changes to this file in the *main* branch.
+  * 
+  * Maintainer: Kaveesha Yalegama
+  */
+
+import chisel3._
+import chisel3.util._
+import chisel3.experimental.BundleLiterals._
+import chisel3.experimental.IO
+
+object coreConfiguration {
+    val robAddrWidth = 5
+}
