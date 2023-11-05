a0f2e8000e651e0b7e391d2d80179923b505ee79
Kaveesha-98 <kaveesha.yalegama@gmail.com>
2023-11-05 20:36:41 +0530

    removing debug ports from main source code class

--- src/main/scala/testbench/system.scala
+++ src/main/scala/testbench/system.scala
@@ -15,6 +15,77 @@ class system extends Module {
   val cpu = Module(new core {
     val registersOut = IO(Output(decode.registersOut.cloneType))
     registersOut := decode.registersOut
+
+
+    // Debug Signals
+
+    val branchOut = IO(Output(new Bundle() {
+      val mispredicted = Bool()
+      val resfired = Bool()
+      val isbranch = Bool()
+      val btbhit = Bool()
+      val fetchsent = Bool()
+      val resPC = UInt(64.W)
+      val decodePC = UInt(64.W)
+      val decodeIns = UInt(32.W)
+      val isRas = Bool()
+      val rasAction = UInt(64.W)
+      val curPC = UInt(64.W)
+      val predRasAction = UInt(64.W)
+      val rasOveride = UInt(64.W)
+      val rasLogicTrigger = UInt(64.W)
+      val btbVal = UInt(64.W)
+    }))
+    branchOut.mispredicted := fetch.misprediction
+    branchOut.resfired := fetch.resfired
+    branchOut.isbranch := fetch.isabranch
+    branchOut.btbhit := fetch.btbhit
+    branchOut.fetchsent := fetch.fetchsent
+    branchOut.resPC := fetch.resPC
+    branchOut.decodePC := decode.decodePC
+    branchOut.decodeIns := decode.decodeIns
+    branchOut.isRas := fetch.isras
+    branchOut.rasAction := fetch.rasAction
+    branchOut.curPC := fetch.curPC
+    branchOut.predRasAction := fetch.predRasAction
+    branchOut.rasOveride := fetch.rasOveride
+    branchOut.rasLogicTrigger := fetch.rasLogicTrigger
+    branchOut.btbVal := fetch.btbVal
+
+    val execOut = IO(Output(new Bundle {
+      val fired = Bool()
+      val instruction = UInt(32.W)
+      val pc = UInt(64.W)
+      val src1 = UInt(64.W)
+      val src2 = UInt(64.W)
+      val writeData = UInt(64.W)
+    }))
+    execOut.fired := exec.fromIssue.fired
+    execOut.instruction := exec.fromIssue.instruction
+    execOut.pc := decode.toExec.pc
+    execOut.src1 := exec.fromIssue.src1
+    execOut.src2 := exec.fromIssue.src2
+    execOut.writeData := exec.fromIssue.writeData
+
+
+    val fetchOut = IO(Output(new Bundle {
+      val fired = Bool()
+      val pc = UInt(64.W)
+    }))
+    fetchOut.fired := fetch.toDecode.fired
+    fetchOut.pc := fetch.toDecode.pc
+
+    val robOut = IO(Output(new Bundle() {
+      val commitFired = Bool()
+      val pc         = UInt(64.W)
+      val interrupt = Bool()
+    }))
+    robOut.commitFired := rob.commit.fired
+    robOut.pc          := rob.commit.mepc
+    robOut.interrupt   := rob.commit.execptionOccured && rob.commit.mcause(63).asBool
+
+    val sampleOut = IO(Output(decode.csrWriteOut.cloneType))
+    sampleOut := decode.csrWriteOut
   })
 
   val memory = Module(new mainMemory)
@@ -28,10 +99,13 @@ class system extends Module {
   val finishedProgramming = IO(Input(memory.finishedProgramming.cloneType))
   memory.finishedProgramming := finishedProgramming
 
-  val peripheralUart = Module(new uart)
+  val peripheralUart = Module(new uart{
+    val putCharOut = IO(Output(putChar.cloneType))
+    putCharOut := putChar
+  })
 
-  val putChar = IO(peripheralUart.putChar.cloneType)
-  putChar <> peripheralUart.putChar
+  val putChar = IO(Output(peripheralUart.putCharOut.cloneType))
+  putChar := peripheralUart.putCharOut
 
   cpu.peripheral <> peripheralUart.client 
 
