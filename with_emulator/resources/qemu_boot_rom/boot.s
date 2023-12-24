d9ad0e5664d847ce019f54aecafd89f8d5a61328
Kaveesha-98 <kaveesha.yalegama@gmail.com>
2023-12-24 13:25:36 +0530

    remove redundant with_emulator

--- with_emulator/resources/qemu_boot_rom/boot.s
+++
@@ -1,9 +0,0 @@
-_start:
-auipc t0, 0;
-addi a2, t0, 40;
-csrrs a0, mhartid, zero;
-ld a1, 32(t0);
-ld t0, 24(t0);
-jr t0;
-kernal_start: .dword 0x0000000080000000;
-dtb_start:    .dword 0x0000000087e00000;
