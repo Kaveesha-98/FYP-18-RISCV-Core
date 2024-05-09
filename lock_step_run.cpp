d38b50374c11cbd23555a4f54ccee9dd1110ba04
MCR748 <mcrlgdous@gmail.com>
2024-05-09 09:42:33 +0530

    Modified lockstep

--- lock_step_run.cpp
+++ lock_step_run.cpp
@@ -16,6 +16,8 @@ using namespace std;
 using namespace std::chrono;
 
 #define LOGGING
+#define DUMP_CONDITION 1 /* && (bench.tickcount > 15800955UL) */
+#define PROBE_DOUBLE (0x08dbf180UL+0x0cUL) & (~7UL)
 
 emulator golden_model;
 
@@ -130,7 +132,7 @@ int main(int argc, char* argv[]) {
   keys_rx.reader = 0;
   keys_rx.writer = 0;
   unsigned long gprs[32];
-  bench.set_probe(0x04782b8UL+0x00U);
+  bench.set_probe(PROBE_DOUBLE);
   bench.step_nodump();
   unsigned long sim_prev = 0x80100000UL;
   while (1 || (bench.tickcount + bench.dump_tick) < 800351768UL) {
@@ -157,28 +159,29 @@ int main(int argc, char* argv[]) {
       outFile << setfill('0') << setw(8) << hex << golden_model.get_csr_value(MIE) << "\n";
       old_symbol = current_symbol;
     } */
-    outFile <<  setfill('0') << setw(16) << dec <<  (bench.tickcount + bench.dump_tick)  << " ";
+    outFile <<  setfill('0') << setw(16) << dec <<  (bench.dump_tick)  << " ";
     outFile <<  setfill('0') << setw(16) << hex << golden_model.get_pc() << " ";
     outFile <<  setfill('0') << setw(16) << hex << golden_model.get_instruction() << " ";
-    outFile <<  setfill('0') << setw(16) << hex << golden_model.fetch_long(0x004782b8UL+0x00U) << " ";
-    outFile <<  setfill('0') << setw(16) << hex << bench.get_probe() << endl;
+    outFile <<  setfill('0') << setw(16) << hex << golden_model.fetch_long(PROBE_DOUBLE) << " ";
+    outFile <<  setfill('0') << setw(16) << hex << bench.get_probe() << " ";
+    outFile <<  setfill('0') << setw(16) << hex << golden_model.reg_file[15] << endl;
     /* switch (golden_model.check_for_mem_access(&mem_address, &data))
     {
     case 1:
       //if (mem_address == 0x80001000) { printf("0x%016lx\n", data); }
-      if (mem_address >= 0x80000000 && mem_address < 0x90000000) { break; }
+      if (mem_address >= 0x80000001 || mem_address < 0x90000000) { break; }
       outFile << "load access at " << setfill('0') << setw(16) << hex << mem_address;
       outFile << " reading data " << setfill('0') << setw(16) << hex << data << "\n";
       break;
     
     case 2:
-      if (mem_address >= 0x80000000 && mem_address < 0x90000000) { break; }
+      if (mem_address >= 0x80000001 || mem_address < 0x90000000) { break; }
       outFile << "store access at " << setfill('0') << setw(16) << hex << mem_address;
       outFile << " writing data " << setfill('0') << setw(16) << hex << data << "\n";
       break;
 
     case 3:
-      if (mem_address >= 0x80000000 && mem_address < 0x90000000) { break; }
+      if (mem_address >= 0x80000001 || mem_address < 0x90000000) { break; }
       outFile << "atomic access at " << setfill('0') << setw(16) << hex << mem_address;
       outFile << " reading data " << setfill('0') << setw(16) << hex << data << "\n";
       break;
@@ -222,7 +225,7 @@ int main(int argc, char* argv[]) {
       golden_model.show_state(); 
       break;
     }
-    /* if (0 && (bench.tickcount >= 856489UL)) {
+    /* if (1 || (bench.tickcount >= 856489UL)) {
       cout << "time out reached" << endl;
       golden_model.show_state(); break;
     } */
@@ -230,17 +233,11 @@ int main(int argc, char* argv[]) {
       cout << "Register mismatch at register " << dec << bench.check_registers(golden_model.reg_file, golden_model.get_mstatus());
       cout << " simulator value: " << setfill('0') << setw(16) << hex << bench.read_register(bench.check_registers(golden_model.reg_file, golden_model.get_mstatus())) << endl;
       golden_model.show_state();
-      cout << dec << (bench.tickcount + bench.dump_tick) << endl;bench.step(); bench.step(); bench.step(); break;
-    }
-    if (bench.check_registersF(golden_model.freg_file) != 33) { 
-      cout << "Register mismatch at fregister " << dec << bench.check_registersF(golden_model.freg_file);
-      cout << " simulator value: " << setfill('0') << setw(8) << hex << bench.read_registerF(bench.check_registersF(golden_model.freg_file)) << endl;
-      golden_model.show_state();
-      cout << dec << (bench.tickcount + bench.dump_tick) << endl;bench.step(); bench.step(); bench.step(); break;
+      cout << dec << (bench.tickcount + bench.dump_tick) << endl;bench.step(); bench.step(); bench.step(); bench.step(); bench.step(); break;
     }
     sim_prev = golden_model.get_pc();
     int x = 1;
-    if (0 && (bench.tickcount > 1032924UL)) {
+    if (DUMP_CONDITION) {
       x = bench.step();
     } else {
       x = bench.step_nodump();
@@ -271,7 +268,7 @@ int main(int argc, char* argv[]) {
       break;
     }
     if (x == 2) { 
-      if (0 && (bench.tickcount > 1032924UL)) {
+      if (DUMP_CONDITION) {
         x = bench.step();
       } else {
         x = bench.step_nodump();
@@ -302,7 +299,8 @@ int main(int argc, char* argv[]) {
   outFile.close();
   #endif
 
-  printf(" Test failed: Time-out!\n");
+  printf("Test failed: Time-out!\n");
+  printf("Total ticks: %ld \n", (bench.tickcount+bench.dump_tick));
   disable_raw_mode();
   tcflush(0, TCIFLUSH); 
 
